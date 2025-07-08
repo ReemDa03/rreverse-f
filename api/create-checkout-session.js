535d63f8d2957235eba22c380b3702c30824b385
ReemDa03 <reemdarwish07@gmail.com>
2025-07-08 04:27:08 +0300

    Fix vercel.json routing after moving project to root

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -72,9 +72,8 @@ export default async function handler(req, res) {
         },
       ],
       mode: "payment",
-      success_url: `${success_url}?payment=success`,
-cancel_url: `${cancel_url}?payment=cancel`,
-
+      success_url,
+      cancel_url,
     });
 
   
